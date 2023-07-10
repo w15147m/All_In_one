2d6e51dcf110fbfffa0816663a393fea8c92c817
lab2 <wa15147m@gmail.com>
2023-07-10 21:06:34 +0500

    home done working on S_C_L_view

--- app/src/main/java/com/example/all_in_one/MainActivity2.java
+++ app/src/main/java/com/example/all_in_one/MainActivity2.java
@@ -3,20 +3,29 @@ package com.example.all_in_one;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
     Toolbar toolbar;
+    CardView cardView1 , cardView2 , cardView3 , cardView4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+//        Hooks
         toolbar = findViewById(R.id.toolbar2);
+        cardView1 = findViewById(R.id.card1);
+        cardView2 = findViewById(R.id.card2);
+        cardView3 = findViewById(R.id.card3);
+        cardView4 = findViewById(R.id.card4);
 //        step 1
         setSupportActionBar(toolbar);
         if(getSupportActionBar()!=null){
@@ -25,7 +34,14 @@ public class MainActivity2 extends AppCompatActivity {
         getSupportActionBar().setSubtitle("Home page");
 
 
-
+           cardView1.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View v) {
+                   Intent intent = new Intent(MainActivity2.this , S_R_L_View.class);
+                   startActivity(intent);
+//                   Toast.makeText(MainActivity2.this, "ali mola", Toast.LENGTH_SHORT).show();
+               }
+           });
 
 
 
@@ -47,13 +63,18 @@ public class MainActivity2 extends AppCompatActivity {
 
         if(itemid==R.id.profile){
 
-            Toast.makeText(this, "profile", Toast.LENGTH_SHORT).show();
+//            Toast.makeText(this, "profile", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(MainActivity2.this , MainActivity.class);
+            startActivity(intent);
         }else if(itemid==R.id.newtab){
 
             Toast.makeText(this, "new", Toast.LENGTH_SHORT).show();
         }else if(itemid==R.id.history){
+            Intent intent = new Intent(MainActivity2.this , S_R_L_View.class);
+            startActivity(intent);
+//            Toast.makeText(this, "history", Toast.LENGTH_SHORT).show();
 
-            Toast.makeText(this, "history", Toast.LENGTH_SHORT).show();
+
         } else if(itemid==R.id.setting){
 
             Toast.makeText(this, "setting", Toast.LENGTH_SHORT).show();
--- app/src/main/java/com/example/all_in_one/S_R_L_View.java
+++ app/src/main/java/com/example/all_in_one/S_R_L_View.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity3 extends AppCompatActivity {
+public class S_R_L_View extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main3);
+        setContentView(R.layout.activity_sr_lview);
     }
 }
